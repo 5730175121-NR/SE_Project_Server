3739df1575cc163ef57e7cd9614b2d2a434c65a9
5730175121-NR <nuttapol7185@gmail.com>
2017-11-14 23:00:31 +0700

    create position class

---
+++ position.py
@@ -0,0 +1,15 @@
+class Position:
+
+    def __init__(self, latitude= 0.0, longitude = 0.0, timestamp= '')
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def isValid(self):
+        #return bool
+    
+    def getUserPosition(self, name):
+        #return list<double,double>
+
+    def getLocationName(latitude, longitude):
+        #return list<string>
+    
