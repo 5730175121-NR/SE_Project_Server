0b615f264932be23cee7bb9628023d33f426bc04
5730175121-NR <nuttapol7185@gmail.com>
2017-11-14 22:52:11 +0700

    create caller class

---
+++ caller.py
@@ -0,0 +1,15 @@
+from user import User
+
+class Caller(User):
+
+    def callTaxi(self):
+        print('call taxi!!')
+
+    def searchTaxi(self):
+        print('return Object')
+
+    def bookTaxi(self):
+        print('booking')
+
+    def paid(transactionID):
+        print('paid')
