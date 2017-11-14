28184b36243481dda3b5099ff93ca17cd83f2dd7
5730175121-NR <nuttapol7185@gmail.com>
2017-11-14 23:48:29 +0700

    create driver class

---
+++ driver.py
@@ -0,0 +1,28 @@
+from user import User 
+
+class Driver:
+    def __init__(isActive= '', status= '', location= '',licenseNO= '', drivePermissionID= '', SSN= ''):
+        self.isActive = isActive
+        self.status = status
+        self.location = location
+        self.licenseNO = licenseNO
+        self.drivePermissionID = drivePermissionID
+        self.SSN = SSN
+
+    def accept(self, transactionID):
+        # return int
+    
+    def pickup(self):
+        # void
+
+    def dropOff(self):
+        # void
+
+    def chat(self):
+        # void
+
+    def readReview():
+        # void
+
+
+    
