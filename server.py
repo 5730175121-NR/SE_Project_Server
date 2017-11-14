8f36974f343ffcebd4b7323d4cbd29894f8b4af8
5730175121-NR <nuttapol7185@gmail.com>
2017-11-14 21:48:25 +0700

    initial

---
+++ server.py
@@ -0,0 +1,29 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn
+import threading
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print(threading.currentThread().getName())
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write("Hello world!!!".encode('utf-8'))       
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = 5000
+    server = ThreadedHTTPServer((host, port), Handler)
+    print('Starting server on %s port: %s, use <Ctrl-C> to stop' % (host, port))
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
+    print('server is closed.')
