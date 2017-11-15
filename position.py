722ba3fd09ec6fc6a1ec81d018e171f3a5293968
Administrator <admin@example.com>
2017-11-15 23:08:25 +0700

    implements setLocation"

--- position.py
+++ position.py
@@ -1,15 +1,23 @@
+import datetime 
+
 class Position:
 
-    def __init__(self, latitude= 0.0, longitude = 0.0, timestamp= '')
+    def __init__(self, latitude=0.0, longitude =0.0, timestamp= ''):
         self.latitude = latitude
         self.longitude = longitude
+        self.timestamp = timestamp
 
     def isValid(self):
-        #return bool
+        return True
     
     def getUserPosition(self, name):
-        #return list<double,double>
+        return ['null']
 
-    def getLocationName(latitude, longitude):
-        #return list<string>
+    def getLocationName(self, latitude, longitude):
+        return 'no place'
+
+    def setLocationName(self, latitude, longitude):
+        self.timestamp = datetime.datetime.now()
+        self.latitude = latitude
+        self.longitude = longitude
     
