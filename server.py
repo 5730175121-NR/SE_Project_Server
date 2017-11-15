3f829c6cf39b266c7df5a47950b59d6256a07174
5730175121-NR <nuttapol7185@gmail.com>
2017-11-15 20:05:50 +0700

    server respone with json

--- server.py
+++ server.py
@@ -10,17 +10,15 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         parsed_path = parse.urlparse(self.path)
-        print(parsed_path)
         path= self.path
         realpath= parsed_path.path
         querys = parsed_path.query
         response = url_management(realpath, querys)
-        print(response)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write("called".encode('utf-8'))       
+        self.wfile.write(json.dumps(response).encode('utf-8'))       
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
